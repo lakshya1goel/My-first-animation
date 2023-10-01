6d8295dd3d26921c6d65512361ec084e4ac0971d
lakshya1goel <118365845+lakshya1goel@users.noreply.github.com>
2023-10-01 22:49:06 +0530

    add lander and moon on landing page

--- landing.js
+++ landing.js
@@ -9,3 +9,42 @@ document.addEventListener('DOMContentLoaded', function() {
 
     moveBackground(); 
 });
+
+document.addEventListener('keydown', function(event) {
+    setTimeout(function() {
+        var rocket = document.querySelector('.rocket-container img');
+        var moon = document.querySelector('.moon-container img');
+        var animationDuration = 1500;
+        var startLeft = 15;  
+        var startBottom = 20; 
+        var endRight = 0;  
+        var endTop = 0;  
+        var stepLeft = (endRight - startLeft) / (animationDuration / 16);
+        var stepBottom = (endTop - startBottom) / (animationDuration / 16);
+
+        function moveRocket() {
+            startLeft -= stepLeft;
+            startBottom -= stepBottom;
+            rocket.style.left = startLeft + '%';
+            rocket.style.bottom = startBottom + '%';
+            console.log(rocket.style.left, rocket.style.bottom);
+
+            setTimeout(function() {
+                var link = document.createElement('a');
+                link.href = './landing.html';
+                link.target = '_blank';
+                document.body.appendChild(link);
+                link.click();
+                document.body.removeChild(link);
+            }, 3000);
+
+            if (startLeft <= endRight && startBottom <= endTop && Math.abs(startLeft - endRight) < 0.1 && Math.abs(startBottom - endTop) < 0.1) {
+                clearInterval(animationInterval);
+            }
+        }
+
+        var animationInterval = setInterval(moveRocket, 10);
+    }, 1000); 
+
+    event.preventDefault();
+});
