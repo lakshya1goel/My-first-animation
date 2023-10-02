ea6720e804158cd93db20c04816e1d051ca65ca6
lakshya1goel <118365845+lakshya1goel@users.noreply.github.com>
2023-10-02 22:54:28 +0530

    give motion to balloons

--- js/celebration.js
+++ js/celebration.js
@@ -4,15 +4,67 @@ document.addEventListener('DOMContentLoaded', function() {
         if (event.key === 'ArrowLeft') {
             var images = document.getElementById('balloons-1');
             images.style.display='inline';
+            var animationDuration = 4000; 
+            var startBottom = 0;
+            var startLeft = 0;  
+            var endBottom = 100; 
+            var endLeft = 40;
+            var stepBottom = (endBottom - startBottom) / (animationDuration / 20);
+            var stepLeft = (endLeft - startLeft) / (animationDuration / 20);
+            function moveLander() {
+                startBottom += stepBottom;
+                startLeft += stepLeft;
+                images.style.bottom = startBottom + '%';
+                images.style.left = startLeft + '%';
+                if (startBottom >= endBottom && startLeft >= endLeft) {
+                    clearInterval(animationInterval);
+                    images.style.display='none';
+                }
+            }
+            var animationInterval = setInterval(moveLander, 20);
         }
+
         if (event.key === 'ArrowRight') {
             var images = document.getElementById('balloons-2');
             images.style.display='inline';
+            var animationDuration = 4000; 
+            var startBottom = 0;
+            var startLeft = 90;  
+            var endBottom = 100; 
+            var endLeft = 40;
+            var stepBottom = (endBottom - startBottom) / (animationDuration / 25);
+            var stepLeft = (endLeft - startLeft) / (animationDuration / 25);
+            function moveLander() {
+                startBottom += stepBottom;
+                startLeft += stepLeft;
+                images.style.bottom = startBottom + '%';
+                images.style.left = startLeft + '%';
+                if (startBottom >= endBottom && startLeft <= endLeft) {
+                    clearInterval(animationInterval);
+                    images.style.display='none';
+                }
+            }
+            var animationInterval = setInterval(moveLander, 20);
         }
+
         if (event.key === 'ArrowUp') {
             var images = document.getElementById('balloons-3');
             images.style.display='inline';
+            var animationDuration = 4000; 
+            var startBottom = 0;
+            var endBottom = 100; 
+            var stepBottom = (endBottom - startBottom) / (animationDuration / 25);
+            function moveLander() {
+                startBottom += stepBottom;
+                images.style.bottom = startBottom + '%';
+                if (startBottom >= endBottom) {
+                    clearInterval(animationInterval);
+                    images.style.display='none';
+                }
+            }
+            var animationInterval = setInterval(moveLander, 20);
         }
+
         if (event.key === 'ArrowDown') {
             counter = (counter % 3) + 1;
             var id = 'cracker-' + counter;
@@ -21,3 +73,6 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
 });
+
+
+
